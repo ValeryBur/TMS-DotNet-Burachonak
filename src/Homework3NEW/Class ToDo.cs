7c9d11ff93118ffb6221a9d6dce20f4b9cad5ae4
ValeryBur <valerik_utd@mail.ru>
2020-10-06 16:34:31 +0300

    feat: Homework 3 is done

---
+++ src/Homework3NEW/Class ToDo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Homework3NEW
+{
+    partial class Program
+    {
+        public class ToDo
+        {
+            private readonly string Id = Guid.NewGuid().ToString().ToUpper().Substring(0, 5);
+            private readonly DateTime _dateTime = DateTime.Now;
+            private ToDoStatus _status = ToDoStatus.Backlog;
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public void SetStatus(ToDoStatus status)
+            {
+                _status = status;
+            }
+            public string GetId()
+            {
+                return Id;
+            }
+            public void GetInfo()
+            {
+                Console.WriteLine();
+                Console.WriteLine(Id);
+                Console.WriteLine("--------------------");
+                Console.WriteLine($"Name: {Name}");
+                Console.WriteLine($"Descripton: {Description}");
+                Console.WriteLine($"Status: {_status}");
+                Console.WriteLine($"DateTime: {_dateTime}");
+            }
+            public ToDoStatus ConvertStatus(string status)
+            {
+                return status switch
+                {
+                    "Backlog" => ToDoStatus.Backlog,
+                    "InProgress" => ToDoStatus.InProgress,
+                    "Done" => ToDoStatus.Done,
+                    "Cancelled" => ToDoStatus.Cancelled,
+                    _ => ToDoStatus.Unknown,
+                };
+            }
+        }
+    }
+}
---
+++ src/Homework3NEW/enum ToDoStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework3NEW
+{
+    public enum ToDoStatus
+    {
+        Unknown = 0,
+        Backlog = 1,
+        InProgress = 2,
+        Done = 3,
+        Cancelled = 4,
+    }
+}
